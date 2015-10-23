4f5dde1570ef678c200325f7abc39b5d1a22a28a
somename <someemail>
2015-10-23 12:05:44 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,15 +3,18 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
 	public void testCreatePlanetWithThreeObstacles() {
 		
-		int x = 10;
-		int y = 10;
+		int x = 100;
+		int y = 100;
 		String obstacles = "(5,5)(9,9),(4,4)";
-		PlanetExplorer planetExplorer = new PlanetExplorer(x,y,);
+		PlanetExplorer planetExplorer = new PlanetExplorer(x,y,obstacles);
+		
+		
 	}
 }
