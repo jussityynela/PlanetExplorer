db90a336777cc29421a5526bcc21250fef318d26
somename <someemail>
2015-10-23 12:08:26 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -7,13 +7,16 @@ import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
+	private static final String empty = "";
+	private static final String landing_position = "(0,0,N)";
 	@Test
-	public void testCreatePlanetWithThreeObstacles() {
+	public void testCreatePlanetWithThreeObstaclesAndReturnValueAfterLanding00N() {
 		
 		int x = 100;
 		int y = 100;
 		String obstacles = "(5,5)(9,9),(4,4)";
 		PlanetExplorer planetExplorer = new PlanetExplorer(x,y,obstacles);
+		AssertEquals( landing_position, planetExplorer.executeCommand(empty) );
 		
 		
 	}
