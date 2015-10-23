a11a92d2635b7d0f2c48a5beadab4cf7eb3321d4
somename <someemail>
2015-10-23 12:04:09 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreatePlanetWithThreeObstacles() {
+		
+		int x = 10;
+		int y = 10;
+		String obstacles = "(5,5)(9,9),(4,4)";
+		PlanetExplorer planetExplorer = new PlanetExplorer(x,y,);
 	}
 }
