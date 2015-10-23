c13618e48b39a86a075f74f549caa4ab86aa683c
somename <someemail>
2015-10-23 13:30:16 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -100,7 +100,7 @@ public class TestPlanetExplorer {
 
 		PlanetExplorer planetExplorer = createPlanetWithThreeObstaclesSize100();
 		//act and assert
-		planetExplorer.executeCommand("b");
+		planetExplorer.executeCommand("f");
 		assertEquals( "(0,1,N)", planetExplorer.executeCommand("b"));
 		
 	}
