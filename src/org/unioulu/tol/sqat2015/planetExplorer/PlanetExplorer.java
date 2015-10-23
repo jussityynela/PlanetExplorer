1e971ed278ed64a48cb975b21d9a25863ea037ac
somename <someemail>
2015-10-23 13:17:56 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -12,6 +12,8 @@ public class PlanetExplorer {
 	private int position_X;
 	private int position_Y;
 	private Orientation orientation;
+	private int planetSize_X;
+	private int planetSize_Y;
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
